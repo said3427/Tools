9d24c87757d10e9fdf393288572c05dda3d96295
Said Muñoz Montero <said3427@gmail.com>
2019-02-02 03:28:54 -0600

    changes in ezbio_ref

--- ezbio_ref.r
+++ ezbio_ref.r
@@ -3,19 +3,21 @@ source("http://bioconductor.org/biocLite.R")
 biocLite("RAM")
 library("RAM")
 setwd("~/Desktop/")
-taxonomy<-data.table::fread("ezbiocloud_id_taxonomy.txt",sep="\t")
-taxonomy<-separate(taxonomy,col="V2",sep = ";",into=c("kingdom","phylum","class","order","family","genus","species"))
-especies<-strsplit(taxonomy$species," ")
+taxonomy<-data.table::fread("taxonomy.tsv",sep="\t")
+taxonomy<-separate(taxonomy,col="Taxon",sep = ";",into=c("kingdom","phylum","class","order","family","genus","species"))
+especies<-strsplit(taxonomy$species,"_")
 nombres<-c()
 for(i in especies){
   if(length(i)>1){
+    if(i[2]=="s"){nombres<-c(nombres,i[1])}else(
     nombres<-c(nombres,i[2])
+    )
     }
   else(
     nombres<-c(nombres,i[1])
     ) 
 }
 
-taxonomy<-within(taxonomy,tax<-paste(kingdom,phylum,class,order,family,genus,species,sep=";"))
-
-write.table(taxonomy[,c("V1","tax")],quote = FALSE,row.names = FALSE,col.names = FALSE,sep="\t",file = "EZBIO_ref.txt")
+taxonomy$species<-nombres
+taxonomy<-within(taxonomy,tax<-paste(paste0("k__",kingdom),paste0("p__",phylum),paste0("c__",class),paste0("o__",order),paste0("f__",family),paste0("g__",genus),paste0("s__",species),sep=";"))
+write.table(taxonomy[,c("Feature ID","tax")],quote = FALSE,row.names = FALSE,col.names = FALSE,sep="\t",file = "EZBIO_ref.txt")
