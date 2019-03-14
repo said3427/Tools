a7d32f005f997f7b3c798932674d9a88160f8831
Said Muñoz Montero <said3427@gmail.com>
2019-03-14 10:21:00 -0600

    example for prophet

--- Stock/Stock.R
+++ Stock/Stock.R
@@ -1,22 +1,28 @@
 #Stock Prophet
 #install.packages("prophet")
+#install.packages("crypto")
+
 library(prophet)
 library(reshape)
+library(crypto)
 
 setwd("~/Github/Tools/Stock/")
-df <- read.csv('stock-price/GOOG.csv')
+cryptocoin<- crypto_history("xlm")
+cryptocoin<- crypto_history("btc")
+#df <- read.csv('stock-price/GOOG.csv')
 
-df<-df[,1:4]
-df.melt<-melt(df)
+df<-cryptocoin[,c(4,6:9)]
+df.melt<-melt(df,id.vars = "date")
 df.melt<-df.melt[,-2]
 colnames(df.melt)<-c("ds","y")
 
 ##Starting Prophet
 m <- prophet(df.melt,daily.seasonality = TRUE)
 
-future <- make_future_dataframe(m, periods = 365)
-tail(future)
+#Crea un vector de fechas futuras
+future <- make_future_dataframe(m, periods = 7)
 
+#Predicción de la crypto currency
 forecast <- predict(m, future)
 tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
 
@@ -24,3 +30,6 @@ tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
 plot(m, forecast)
 
 prophet_plot_components(m, forecast)
+
+plot(m, forecast) + add_changepoints_to_plot(m)
+
